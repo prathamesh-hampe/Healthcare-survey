5518b6178b143cdd3df909a1d03684ba7f443d03
prathamesh-hampe <prathmeshhampe@gmail.com>
2026-03-22 23:21:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const fs = require("fs-extra");
 
 const app = express();
 app.use(express.json());
-app.use(express.static("public"));
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const FILE = "./data/responses.json";
 
